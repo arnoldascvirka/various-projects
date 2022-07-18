e853edfd2d3e94dc5eef3affb6707f183bdbfa50
arnoldascvirka <nvaeterrae@gmail.com>
2022-07-18 23:08:52 +0300

    C_0?

---
+++ CC1/C_09/A1.py
@@ -0,0 +1,12 @@
+class Zmogus:
+    def __init__(self, vardas, amzius):
+        self.vardas = vardas
+        self.amzius = amzius
+
+    def begti(self):
+        print("begu")
+
+
+zmogus1 = Zmogus("Jonas", "15")
+
+zmogus1.begti()
