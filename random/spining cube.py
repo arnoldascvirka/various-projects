a85bb5d3ff5adbbdfb69695055f44077f571fcda
Arnoldas <71648212+arnoldascvirka@users.noreply.github.com>
2022-12-12 23:36:23 +0200

    spining cube

--- random/spining cube.py
+++ random/spining cube.py
@@ -13,12 +13,19 @@ z = [0,0,0,0,1,1,1,1]
 #draw cube
 ax.scatter(x, y, z)
 
+#static points inside cube
+points = [(0.25, 0.25, 0.25), (0.75, 0.75, 0.75), (0.25, 0.75, 0.75), 
+          (0.75, 0.25, 0.75), (0.75, 0.75, 0.25), (0.25, 0.25, 0.75),
+          (0.25, 0.75, 0.25), (0.75, 0.25, 0.25), (0.5, 0.5, 0.5)]
+for point in points:
+    ax.scatter(point[0], point[1], point[2], c='r')
 
 #misc
-plt.xlabel(None)
-plt.ylabel(None)
+ax.set_xticks([])
+ax.set_yticks([])
 plt.xticks([])
 plt.yticks([])
+ax.set_yticklabels([])
 
 #spin cube
 
