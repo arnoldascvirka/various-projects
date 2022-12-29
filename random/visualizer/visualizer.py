14ffe8a4910f2d0de0373befd597975ca756800a
Arnoldas <71648212+arnoldascvirka@users.noreply.github.com>
2022-12-29 13:03:18 +0200

    Update visualizer.py

--- random/visualizer/visualizer.py
+++ random/visualizer/visualizer.py
@@ -12,12 +12,15 @@ stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,
 app = pg.QtWidgets.QApplication([])
 plot = pg.plot()
 plot.setXRange(0, 1024)
-plot.setYRange(-32768, 32768)
+plot.setYRange(-30000, 30000)\
+
 
 # Start the audio stream and visualizer loop
 while True:
     data = np.fromstring(stream.read(1024), dtype=np.int16)
     plot.plot(data, clear=True)
+    plot.getPlotItem().hideAxis('bottom')
+    plot.getPlotItem().hideAxis('left')
     app.processEvents()
 
 # Clean up PyAudio
