7a54a8bac33c6f6b04c68d4a46909a1f530f1bf5
Arnoldas <71648212+arnoldascvirka@users.noreply.github.com>
2022-12-29 11:59:48 +0200

    C

---
+++ random/visualizer/visualizer.py
@@ -0,0 +1,26 @@
+import pyaudio
+import numpy as np
+import pyqtgraph as pg
+import sounddevice as sd
+
+# Set up PyAudio
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True,
+                frames_per_buffer=1024)
+
+# Set up PyQtGraph
+app = pg.QtWidgets.QApplication([])
+plot = pg.plot()
+plot.setXRange(0, 1024)
+plot.setYRange(-32768, 32768)
+
+# Start the audio stream and visualizer loop
+while True:
+    data = np.fromstring(stream.read(1024), dtype=np.int16)
+    plot.plot(data, clear=True)
+    app.processEvents()
+
+# Clean up PyAudio
+stream.stop_stream()
+stream.close()
+p.terminate()
